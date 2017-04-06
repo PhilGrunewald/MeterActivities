f8c7bb1eed7052c05553b5458aaec35941afd2b6
Phil Grunewald <philipp.grunewald@ouce.ox.ac.uk>
2017-04-06 23:06:00 +0100

    using SQL Legend

--- utils/cockpit.py
+++ utils/cockpit.py
@@ -14,36 +14,43 @@ import db_ini as db     # reads the database and file path information
 
 SQLquery ={
 "PDF": "\
-        SELECT {0},ROUND\
-            (\
-            COUNT(*)/\
-                (\
-                SELECT COUNT(*) as count \
-                FROM Household\
-                )\
-            *100\
-            ) as Percent \
-        FROM {1} \
-        GROUP BY {0};",
+Select value,meaning,percent FROM \
+(SELECT {0} AS col,ROUND \
+    ( \
+     COUNT(*)/ \
+         ( \
+         SELECT COUNT(*) as count  \
+         FROM {1} \
+         WHERE {2} \
+         ) \
+     *100 \
+     ,1) as percent  \
+    FROM {1}  \
+    WHERE {2} \
+    GROUP BY col) as ColPercent \
+JOIN Legend \
+    ON col = Legend.`value` \
+    WHERE Legend.`column` = '{0}' \
+    AND Legend.`table` = '{1}' \
+    ORDER BY value;",
+
+"PDF_noLegend": "\
+    Select value, percent  \
+	FROM (SELECT {0} AS value,ROUND     (       \
+    COUNT(*)/          (          SELECT COUNT(*) as count            \
+    FROM {1}          WHERE {2}          )      *100      ,1) as percent       \
+    FROM {1}      WHERE {2}      \
+    GROUP BY value) as ColPercent \
+    ORDER BY value;",
+
 "Count": "\
     SELECT Count(*) as result\
-    FROM {};"
+    FROM {0}\
+    WHERE {1};"
 }
 
-with open('Definitions.json') as f:    
-    Definitions = json.load(f)
 
-# Definitions = {
-# "Gender": { 
-#         "0": "Skip",
-#         "1": "Female",
-#         "2": "Male",
-#         "3": "Other"
-#         }
-# }
 # ========= #
-        # \
-        # ORDER BY {0};"
 # FUNCTIONS #
 # ========= #
 
@@ -57,38 +64,8 @@ def connectDB():
         cursorclass=MySQLdb.cursors.DictCursor)
     return dbConnection.cursor()
 
-def getResults(_query,col):
-    """ send sql query and return result as list """
-    reverse = False
-    resultStr = ''
-    if (col in Definitions):
-        resultStr += "\n\n{0}\n\n".format(Definitions[col]['q'])
-        
-    cursor.execute(_query)
-    results = cursor.fetchall()
-    ks = results[0].keys()
-    if (ks[0] != col):
-        # make sure the column comes first
-        ks = reversed(ks)
-        reverse = True
-    resultStr += '\t|'.join("{}".format(str(e)) for e in ks)
-    resultStr += "\n:--- |---:\n"
-        
-    for result in results:
-        vs = result.values()
-        a = 0
-        b = 1
-        if reverse:
-            a = 1
-            b = 0
-        if (col in Definitions):
-            resultStr += "{0}\t|{1}\n".format(Definitions[col][str(vs[int(a)])], vs[b])
-        print resultStr
-    return resultStr
-
-
-def getCount(table):
-    sqlq = SQLquery['Count'].format(table)
+def getCount(table,condition):
+    sqlq = SQLquery['Count'].format(table,condition)
     cursor.execute(sqlq)
     results = cursor.fetchall()
     return results[0]['result']
@@ -104,16 +81,29 @@ def getCols(table):
         colNames.append(result['Field'])
     return colNames
 
-def getColPDF(col,table):
+def getColPDF(col,table,condition):
     """ list all values and percentage of occurances """
-    sqlq = SQLquery['PDF'].format(col,table)
-    return getResults(sqlq,col)
+    cursor.execute(SQLquery['PDF'].format(col,table,condition))
+    results = cursor.fetchall()
+    q = '# Undefined \n\n'
+    row = "{}\t|{}\n:--- |---:\n".format(col,'Percent')
+    for result in results:
+        if (result['value'] == 'q'):
+            q = "# {}\n\n".format(result['meaning'])
+        else:
+            row += "{}\t|{}\n".format(result['meaning'],result['percent'])
+    if (len(results) < 2):
+        cursor.execute(SQLquery['PDF_noLegend'].format(col,table,condition))
+        results2 = cursor.fetchall()
+        for result in results2:
+            row += "{}\t|{}\n".format(result['value'],result['percent'])
+    return "\n{}\n{}".format(q,row)
 
-def getTablePDFs(table):
+def getTablePDFs(table,condition):
     """ go through all cols and return PDF """
     PDF_Str = ""
     for col in getCols(table):
-        PDF =  getColPDF(col,table)
+        PDF =  getColPDF(col,table,condition)
         if (PDF.count('\n') < 18):
             # Formatting for markdown table: 2 col - align right
             # inserted after header (i.e after first \n)
@@ -121,26 +111,26 @@ def getTablePDFs(table):
             PDF_Str += PDF
     return PDF_Str
 
-def createCockpit(table,output):
+def createCockpit(table,condition,output):
     """ produce summary for table stats """
     if output:
         with open(output,'w') as f:
            f.write("% Table: {}\n".format(table))
-           f.write("% Count: {}\n".format(getCount(table)))
-           f.write(getTablePDFs(table))
+           f.write("% Count: {}\n".format(getCount(table,condition)))
+           f.write(getTablePDFs(table,condition))
 
 def main(argv):
     """ Check for arguments """
 
     # Default values
     global width
-    width = 0
     output = False
-    # table = "Household"
-    table = "Individual"
+    table = "Household"
+    condition = "True"
+    # table = "Individual"
 
     # Optional arguments
-    options = "hlt:w:o:"
+    options = "hlt:c:o:"
     optionsLong = ["help","file"]
 
     # Help
@@ -162,17 +152,16 @@ def main(argv):
             sys.exit()
         elif opt in ("-l", "--localhost"):
             db.Host = 'localhost'
-        elif opt in ("-w", "--width"):
-            width = arg
+        elif opt in ("-c", "--condition"):
+            condition = arg
         elif opt in ("-t", "--table"):
             table = arg
         elif opt in ("-o", "--output"):
             output = arg
 
-
     global cursor
     cursor = connectDB()
-    createCockpit(table,output)
+    createCockpit(table,condition,output)
     print "Entry complete\n\n"
 
 # ========= #
